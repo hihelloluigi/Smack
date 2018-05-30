70cc633f2115ff176a3c2586a8c24d4dc98eadf1
Unknown <luigiaiello97@gmail.com>
2018-05-30 12:55:05 +0200

    Added layouts

---
+++ app/src/main/java/com/example/luigi/smack/CreateUserActivity.kt
@@ -0,0 +1,28 @@
+package com.example.luigi.smack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    //region Actions
+    fun generateUserAvatar(view: View) {
+
+    }
+
+    fun generateBackgroundColorClicked(view: View) {
+
+    }
+
+    fun  createUserClicked(view: View) {
+
+    }
+    //endregion
+
+}
---
+++ app/src/main/java/com/example/luigi/smack/LoginActivity.kt
@@ -0,0 +1,25 @@
+package com.example.luigi.smack
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    //region Actions
+    fun loginLoginButtonClicked(view: View) {
+
+    }
+    fun loginCreateUserButtonClicked(view: View) {
+
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
+    }
+    //endregion
+}
